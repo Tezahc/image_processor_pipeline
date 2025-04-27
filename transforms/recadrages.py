dd9af1c1074ea144d8e063eb731a8101de638c98
Tezahc <tezahc23@gmail.com>
2025-04-28 01:04:16 +0200

    rework basic crop function - Add : now accept percent or int value fir each margin to crop - todo : add docstring

--- transforms/recadrages.py
+++ transforms/recadrages.py
@@ -1,42 +1,31 @@
+from pathlib import Path
 import cv2
-import os
 
-# Chemin des photos
-input_folder = r"F:\DeepValve\Bionector\Overlays\Videos\IMAGES_0150"
-output_folder = r"F:\DeepValve\Bionector\Overlays\Videos\CROPPED_0150"
-os.makedirs(output_folder, exist_ok=True)  # Crée le dossier de sortie s'il n'existe pas
 
-# Pourcentages à recadrer (en % des dimensions de l'image)
-crop_top_percent = 30    # Supprime 10% en haut
-crop_bottom_percent = 15  # Supprime 10% en bas
-crop_left_percent = 5    # Supprime 10% à gauche
-crop_right_percent = 5   # Supprime 10% à droite
+def crop_from_border(filename: Path, options: tuple = (0, 0, 0, 0)):
+    # Filtrer les fichiers .jpg
+    if not filename.suffix == '.jpg':
+        raise ValueError("Le Fichier n'est pas du bon type")
 
-# Parcourir toutes les images du dossier
-for image_name in os.listdir(input_folder):
-    if image_name.endswith('.jpg'):  # Filtrer les fichiers .jpg
-        image_path = os.path.join(input_folder, image_name)
-        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    crop_top, crop_bottom, crop_left, crop_right = options
 
-        if image is None:
-            print(f"Erreur : Impossible de charger l'image {image_name}.")
-            continue
+    # lecture de l'image
+    image = cv2.imread(str(filename), cv2.IMREAD_UNCHANGED)
 
-        # Dimensions de l'image
-        height, width = image.shape[:2]
+    if image is None:
+        print(f"Erreur : Impossible de charger l'image {filename}.")
 
-        # Calcul des pixels à recadrer
-        crop_top = int(height * crop_top_percent / 100)
-        crop_bottom = int(height * crop_bottom_percent / 100)
-        crop_left = int(width * crop_left_percent / 100)
-        crop_right = int(width * crop_right_percent / 100)
+    # Dimensions de l'image
+    height, width = image.shape[:2]
 
-        # Recadrage de l'image
-        cropped_image = image[crop_top:height - crop_bottom, crop_left:width - crop_right]
+    # Calcul des pixels à recadrer
+    # TODO: gérer les % ET les valeurs en px
+    crop_top = int(height * crop_top) if 0 <= crop_top < 1 else crop_top
+    crop_bottom = int(height * crop_bottom) if 0 <= crop_bottom < 1 else crop_bottom
+    crop_left = int(width * crop_left) if 0 <= crop_left < 1 else crop_left
+    crop_right = int(width * crop_right) if 0 <= crop_right < 1 else crop_right
 
-        # Sauvegarder l'image recadrée
-        output_path = os.path.join(output_folder, image_name)
-        cv2.imwrite(output_path, cropped_image)
-        print(f"Image recadrée et sauvegardée : {output_path}")
+    # Recadrage de l'image
+    cropped_image = image[crop_top:height - crop_bottom, crop_left:width - crop_right]
 
-print(f"Traitement terminé. Les images recadrées sont enregistrées dans : {output_folder}")
+    return cropped_image
