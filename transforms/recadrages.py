74dcbe9d66c7a470dd745aac682d74c7b1e3b266
Tezahc <tezahc23@gmail.com>
2025-04-28 09:53:08 +0200

    add security check and some renaming

--- transforms/recadrages.py
+++ transforms/recadrages.py
@@ -1,17 +1,23 @@
 from pathlib import Path
+from typing import Tuple
 import cv2
 
 
-def crop_from_border(filename: Path, options: tuple = (0, 0, 0, 0)):
-    # Filtrer les fichiers .jpg
-    if not filename.suffix == '.jpg':
-        raise ValueError("Le Fichier n'est pas du bon type")
+def compute_crop(value, total_length):
+    if value < 0:
+        raise ValueError("Crop value cannot be negative")
+    return int(total_length * value) if 0 <= value < 1 else int(value)
 
-    crop_top, crop_bottom, crop_left, crop_right = options
+
+def crop_from_border(filename: Path, crop_margins: Tuple[float, float, float, float] = (0, 0, 0, 0)):
+    # Filtrer les fichiers .jpg
+    if filename.suffix.lower() not in ('.jpg', '.jpeg'):
+        raise ValueError(f"Le Fichier {filename.name} n'est pas du type JPG.")
+
+    crop_top, crop_bottom, crop_left, crop_right = crop_margins
 
     # lecture de l'image
     image = cv2.imread(str(filename), cv2.IMREAD_UNCHANGED)
-
     if image is None:
         print(f"Erreur : Impossible de charger l'image {filename}.")
 
@@ -19,13 +25,15 @@ def crop_from_border(filename: Path, options: tuple = (0, 0, 0, 0)):
     height, width = image.shape[:2]
 
     # Calcul des pixels à recadrer
-    # TODO: gérer les % ET les valeurs en px
-    crop_top = int(height * crop_top) if 0 <= crop_top < 1 else crop_top
-    crop_bottom = int(height * crop_bottom) if 0 <= crop_bottom < 1 else crop_bottom
-    crop_left = int(width * crop_left) if 0 <= crop_left < 1 else crop_left
-    crop_right = int(width * crop_right) if 0 <= crop_right < 1 else crop_right
+    crop_top_px = compute_crop(crop_top, height)
+    crop_bottom_px = compute_crop(crop_bottom, height)
+    crop_left_px = compute_crop(crop_left, width)
+    crop_right_px = compute_crop(crop_right, width)
 
-    # Recadrage de l'image
-    cropped_image = image[crop_top:height - crop_bottom, crop_left:width - crop_right]
+    if crop_top_px + crop_bottom_px >= height or crop_left_px + crop_right_px >= width:
+        raise ValueError("Les marge de rognages sont trop grandes pour l'image")
+
+    # Recadrage
+    cropped_image = image[crop_top_px:height - crop_bottom_px, crop_left_px:width - crop_right_px]
 
     return cropped_image
