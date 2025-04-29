f679ee31c3a5420069ebb2f6e5a911c7343356c2
Tezahc <tezahc23@gmail.com>
2025-04-29 21:53:05 +0200

    Add function to change class_id of a label file powered by BrainAI ! :proud:

---
+++ transforms/labels.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Any, List, Optional
+from icecream import ic
+
+
+def change_label_class(input_path: Path,
+                       output_dirs: List[Path],
+                       # **options
+                       new_label_class_id: int = 0,
+                       **options: Any
+                       ) -> Optional[Path]:
+    """
+    Docstring
+    """
+
+    # Vérifications : 
+    # if not ouput_dirs => devrait être géré par l'orchestrateur
+    ouput_dir = output_dirs[0]
+
+    try:
+        with input_path.open('r+', encoding='utf-8') as f:
+            line = f.readline()
+            f.seek(0)
+            f.truncate()
+            line = line.split()
+            line[0] = str(new_label_class_id)
+            f.write(' '.join(line))
+        return input_path
+    except Exception as e:
+        print(f"Problème : {e}")
+        return None
+
+if __name__ == '__main__':
+    #tests
+    file = Path(r"C:\Users\GuillaumeChazet\Documents\ICUREsearch\DeepValve\MicroClave_001_h_r047.txt")
+    output = [Path.cwd()]
+
+    change_label_class(file, output, 12)
